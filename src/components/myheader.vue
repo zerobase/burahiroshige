9b2f696eae2395c69bf50f7ca118fdf31a5d3de0
Hide Ishi <hidetoi@gmail.com>
2019-09-15 20:35:43 +0900

    GitHub Cornerを追加
    http://tholman.com/github-corners/

--- src/components/myheader.vue
+++ src/components/myheader.vue
@@ -1,8 +1,19 @@
 <template>
   <nav class="navbar navbar-light bg-light global-navbar">
     <router-link class="nav-title" to="/">ブラ広重(仮)</router-link>
+    <GitHubCorner></GitHubCorner>
   </nav>
 </template>
+
+<script>
+import GitHubCorner from '@/components/GitHubCorner'
+
+export default {
+  components: {
+    GitHubCorner
+  }
+}
+</script>
 
 <style scoped>
 .navbar {
